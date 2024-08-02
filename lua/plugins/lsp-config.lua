a91aa34a2ceeb10a0be0097f81b7f1420f554281
Tsang <t256878g@tdsynnex.com>
2024-08-01 21:20:21 -0400

    first commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,32 @@
+return {
+   {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls"
+                }
+            })
+        end
+    },
+    {
+        "williamboman/nvim-lsp-installer",
+        config = function()
+            require("nvim-lsp-installer").setup({})
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        end
+    }
+}
